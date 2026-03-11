3736b4ef8913ec167760356ab2e833c6b4a6675c
Pakhi Dubey <pakhidubey83@gmail.com>
2026-03-12 00:09:43 +0530

    initial commit

---
+++ conditional/node.js
@@ -0,0 +1,14 @@
+var grade='A';
+document.writeln("entering switch case"+"<br/>");
+switch(grade){
+    case'A':document.writeln("good job"+"<br/>");
+    break;
+    case 'B':document.writeln("pretty good"+"<br/>");
+    break;
+    case 'C':document.writeln("passed"+"<br/>");
+    break;
+    case 'D':document.writeln("not so good"+"<br/>");
+    break;
+    default:document.writeln("failed");
+}
+
