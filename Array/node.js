529638ee5b26fad5d6f63ba55d7b0deefd38fa01
Pakhi Dubey <pakhidubey83@gmail.com>
2026-03-18 22:55:38 +0530

    added

---
+++ Array/node.js
@@ -0,0 +1,6 @@
+var myInfo=new Array();
+var myInfo1=Array();
+console.log(myInfo,myInfo1);
+
+var trainerInfo=["Jintendra chauhan","29","Ghaziabad",(isMarries="true")];
+document.writeln(trainerInfo);
--- function2/node.js
+++ function2/node.js
@@ -5,5 +5,5 @@ var add2=(a,b)=>{
     document.writeln(a+b);
 };
 add2(102,20);
-var add3=(a,b)=>document.writeln(a+b);
+var add3=(a,b)=>{document.writeln(a+b);};
 add3(99+100);
