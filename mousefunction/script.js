83b4cd7512928098cd9a959e689e02985a9810aa
Pakhi Dubey <pakhidubey83@gmail.com>
2026-03-18 23:18:12 +0530

    added

---
+++ mousefunction/script.js
@@ -0,0 +1,18 @@
+function OnClick(){
+    console.log("clicked");
+ }
+ function OnMouseUp(){
+    console.log("on mouse up");
+ }
+ function OnMouseDown(){
+    console.log("on mouse down");
+ }
+ function OnMouseOver(){
+    console.log("mouse over");
+ }
+ function OnMouseRemove(){
+    console.log("mouse remove");
+ }
+ function OnMouseOut(){
+    console.log("mouse out");
+ }
